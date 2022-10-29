3e1cd35e68b8dd150dac1856487f98c4630b33ce
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-30 01:06:23 +0300

    notiflix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
 const axios = require('axios').default;
 
 const API_key = '30945884-5d04be7201908102dc9a782a9';
@@ -20,9 +21,12 @@ async function handleSubmit(event) {
     elements: { searchQuery },
   } = event.currentTarget;
   searchParam = searchQuery;
+
   try {
     const data = await fetchPictures(searchQuery);
-    const rendered = await renderGallery(data);
+    console.log(data);
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    const rendered = await renderGallery(data.hits);
     gallery.innerHTML = rendered.join('');
     lightbox = new SimpleLightbox('.gallery a');
     loadMoreBtn.classList.remove('visually-hidden');
@@ -49,13 +53,14 @@ async function fetchPictures(search) {
   const imgs = await axios.get(url);
   console.log(imgs.data.hits);
   // const imgs = await response.data.json();
-  console.log(`"Hooray! We found ${imgs.totalHits} images."`);
 
   if (imgs.totalHits / 40 < pageNumber - 1) {
-    console.log("We're sorry, but you've reached the end of search results.");
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
     return;
   }
-  return imgs.data.hits;
+  return imgs.data;
   // return imgs.hits.map(img => {
   //   return {
   //     webformatURL: img.webformatURL,
@@ -71,7 +76,7 @@ async function fetchPictures(search) {
 
 async function renderGallery(array) {
   if (!array.length) {
-    console.log(
+    Notiflix.Notify.warning(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     gallery.innerHTML = '';
