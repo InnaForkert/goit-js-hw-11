5c3fe70be50d9f8b6443f59a4793a685d2f69450
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-29 16:32:39 +0000

    Deploying to gh-pages from @ InnaForkert/goit-js-hw-11@429735224184223d8cc9366ffc3e750fcbf8160b 🚀

--- index.6ce0effb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();let{elements:{searchQuery:t}}=e.currentTarget;console.log(t.value)}));
-//# sourceMappingURL=index.6ce0effb.js.map
--- index.fa26f6b4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="30945884-5d04be7201908102dc9a782a9";"https://pixabay.com/api/?key=".concat(e,"&q=").concat(e,"&image_type=photo&orientation=horizontal&safesearch=true");document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements.searchQuery;console.log(t.value)}))}();
-//# sourceMappingURL=index.fa26f6b4.js.map
