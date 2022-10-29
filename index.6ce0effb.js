dbc551faf7d7b79a2f17012d1efa88ca4a22ce4a
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-10-29 11:47:43 +0000

    Deploying to gh-pages from @ InnaForkert/goit-js-hw-11@c4dd71252720dc2d43f77e182f27105b62362216 🚀

---
+++ index.6ce0effb.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();let{elements:{searchQuery:t}}=e.currentTarget;console.log(t.value)}));
+//# sourceMappingURL=index.6ce0effb.js.map
